3e665c0fafe1d0d18fe63d8335878dc5bfcc8b43
ArsyadCyber <124241369+ArsyadCyber@users.noreply.github.com>
2024-03-03 11:08:20 +0000

    m

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 const { main } = require('./index'); // Pastikan path benar
 
 // Middleware untuk melayani file JSON
@@ -7,9 +8,10 @@ app.use('/api/jadwal', express.static('jadwaltv.json'));
 
 // Jalankan fungsi main untuk melakukan scraping saat server dijalankan
 main().then(() => {
-  console.log('Scraping selesai.');
+  console.log('Scraping selesai, server siap dijalankan.');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }).catch(error => {
   console.error('Terjadi kesalahan saat melakukan scraping:', error);
 });
-
-module.exports = app; // Ekspor aplikasi untuk Vercel
